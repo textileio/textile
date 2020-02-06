d38288860b4e72ea25b71abba5fe51376fd5ee90
andrew <andrew@textile.io>
2020-02-05 16:29:04 -0800

    tests: revert to dev mode for ci
    Signed-off-by: andrew <andrew@textile.io>

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -12,20 +12,13 @@ describe('API', function() {
   describe('create new instance', () => {
     it('it should create a new API instance', async () => {
       api = new API({
-        token: 'b8053ec8-456c-47eb-9ee9-99fef7ae812f',
-        deviceId: '77b7b712-5ec4-42f2-b3c4-57b22f0cf057',
+        token: '<app token>',
+        deviceId: '<user id>',
         dev: true,
         api: '127.0.0.1',
         apiScheme: 'http',
       })
       api.start()
-      // api = new API({
-      //   token: '<app token>',
-      //   deviceId: '<user id>',
-      //   dev: true,
-      //   api: '127.0.0.1',
-      //   apiScheme: 'http',
-      // })
       expect(api).to.not.be.undefined
     })
     it('create new store', async () => {
