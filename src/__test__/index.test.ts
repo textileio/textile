6c10464748349e4a2f91513ebae418959e9811f9
andrew <andrew@textile.io>
2020-02-14 12:22:29 -0700

    tests: move tests to dev
    Signed-off-by: andrew <andrew@textile.io>

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -8,18 +8,15 @@ import { Client } from '@textile/threads-client'
 import { expect } from 'chai'
 import { API } from '../index'
 
-const API_TOKEN = `${process.env.TXTL_JS_TEST_TOKEN}`
-const DEVICE_ID = `77d6b032-4f5c-11ea-b77f-2e728ce88125`
-
 describe('API', function() {
   let api: API
   let client: Client
   describe('create new instance', () => {
     it('it should create a new API instance', async () => {
       api = new API({
-        token: API_TOKEN,
-        deviceId: DEVICE_ID,
-        dev: false,
+        token: 'API_TOKEN',
+        deviceId: 'DEVICE_ID',
+        dev: true,
       })
       await api.start()
       expect(api).to.not.be.undefined
