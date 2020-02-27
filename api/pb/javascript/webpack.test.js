0592e1fdc757bca8b602dca71fe108c49f0220db
andrew <andrew@textile.io>
2020-02-27 14:49:13 -0800

    Adds JS client directly in build
    Signed-off-by: andrew <andrew@textile.io>

---
+++ api/pb/javascript/webpack.test.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
