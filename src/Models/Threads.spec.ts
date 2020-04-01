a1c8c2e3fda62a84131bf81f90981b4f2fb24afc
andrew <andrew@textile.io>
2020-04-01 15:29:47 -0700

    fix: add missing spec files
    Signed-off-by: andrew <andrew@textile.io>

---
+++ src/Models/Threads.spec.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable import/first */
+// Some hackery to get WebSocket in the global namespace on nodejs
+// @todo: Find a nicer way to do this...
+;(global as any).WebSocket = require('isomorphic-ws')
+
+import { Client } from '@textile/threads-client'
+import { expect } from 'chai'
+import API from '../API'
+
+describe('ThreadsConfig', function() {
+  let api: API
+  let client: Client
+  describe('In dev-mode, API should be on HTTP on port 6007', () => {
+    it('threadApiScheme should be HTTP', async () => {
+      api = new API({
+        token: 'API_TOKEN',
+        deviceId: 'DEVICE_ID',
+        dev: true,
+      })
+      await api.start()
+      const config = api.threadsConfig
+      expect(config.threadApiScheme).equals('http')
+      client = new Client(api.threadsConfig)
+      expect(client.config.host).contains('http')
+    })
+    it('threadPort should be 6007', async () => {
+      api = new API({
+        token: 'API_TOKEN',
+        deviceId: 'DEVICE_ID',
+        dev: true,
+      })
+      await api.start()
+      const config = api.threadsConfig
+      expect(config.threadsPort).equals(6007)
+      client = new Client(api.threadsConfig)
+      expect(client.config.host).contains('6007')
+    })
+  })
+  describe('Not in dev-mode, API should be on HTTPS on port 6447', () => {
+    it('threadApiScheme should be HTTPS', async () => {
+      api = new API({
+        token: '54e24fc3-fda5-478a-b1f7-040ea5aaab33',
+        deviceId: 'c8e5b368-6d08-4215-81b3-eb2522e2df32',
+        dev: false,
+      })
+      await api.start()
+      const config = api.threadsConfig
+      client = new Client(config)
+      expect(config.threadApiScheme).equals('https')
+      expect(client.config.host).contains('https')
+    })
+    it('threadPort should be 6447', async () => {
+      api = new API({
+        token: '54e24fc3-fda5-478a-b1f7-040ea5aaab33',
+        deviceId: 'c8e5b368-6d08-4215-81b3-eb2522e2df32',
+        dev: false,
+      })
+      await api.start()
+      const config = api.threadsConfig
+      client = new Client(config)
+      expect(config.threadsPort).equals(6447)
+      expect(client.config.host).contains('6447')
+    })
+  })
+})
