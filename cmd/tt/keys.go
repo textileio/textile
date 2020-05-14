896e51d017d4f2717f5877be8fbd5cac93424977
Steven Adger <adgitate@gmail.com>
2020-05-14 13:44:08 -0700

    Fix typo

--- cmd/tt/keys.go
+++ cmd/tt/keys.go
@@ -39,7 +39,7 @@ Using the '--org' flag will create a new key under the Organization's account.
 
 There are two types of API keys:
 1. 'Account' keys provide direct access to developer/org account buckets and threads.
-1. 'User' keys provide existing external identities (users) access to their own buckets and threads, under the custodianship of the parent account.  
+2. 'User' keys provide existing external identities (users) access to their own buckets and threads, under the custodianship of the parent account.  
 
 API secrets should be kept safely on a backend server, not in publicly readable client code.
 `,
