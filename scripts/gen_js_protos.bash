3da605ff488ca0047fbe58e2c981b29194cad127
Andrew Hill <andrew@textile.io>
2020-11-30 15:56:15 -0800

    chore(build): alternate js proto compile method
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- scripts/gen_js_protos.bash
+++ scripts/gen_js_protos.bash
@@ -15,15 +15,16 @@ done
 
 echo generating node-protos in api/hubd/pb/javascript
 grpc_tools_node_protoc \
-  --js_out=import_style=commonjs,binary:api/hubd/pb/javascript \
-  --grpc_out=grpc_js:api/hubd/pb/javascript \
-  --plugin=protoc-gen-ts=api/usersd/pb/javascript/node_modules/.bin/protoc-gen-ts \
-  --ts_out=api/hubd/pb/javascript \
-  -I ./ \
+    -I=. \
+    --plugin=protoc-gen-ts=api/hubd/pb/javascript/node_modules/.bin/protoc-gen-ts \
+    --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+    --js_out=import_style=commonjs:api/hubd/pb/javascript \
+    --grpc_out=grpc_js:api/hubd/pb/javascript \
+    --ts_out=service=grpc-node,mode=grpc-js:api/hubd/pb/javascript \
   api/billingd/pb/billingd.proto api/hubd/pb/hubd.proto
 
 echo generating web-protos in api/hubd/pb/javascript/browser
-mkdir api/hubd/pb/javascript/browser 2>/dev/null
+mkdir -p api/hubd/pb/javascript/browser
 ./buildtools/protoc/bin/protoc \
   --proto_path=. \
   --plugin=protoc-gen-ts=api/hubd/pb/javascript/node_modules/.bin/protoc-gen-ts \
@@ -33,15 +34,16 @@ mkdir api/hubd/pb/javascript/browser 2>/dev/null
 
 echo generating node-protos in api/usersd/pb/javascript
 grpc_tools_node_protoc \
-  --js_out=import_style=commonjs,binary:api/usersd/pb/javascript \
-  --grpc_out=grpc_js:api/usersd/pb/javascript \
-  --plugin=protoc-gen-ts=api/usersd/pb/javascript/node_modules/.bin/protoc-gen-ts \
-  --ts_out=api/usersd/pb/javascript \
-  -I ./ \
+    -I=. \
+    --plugin=protoc-gen-ts=api/usersd/pb/javascript/node_modules/.bin/protoc-gen-ts \
+    --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+    --js_out=import_style=commonjs:api/usersd/pb/javascript \
+    --grpc_out=grpc_js:api/usersd/pb/javascript \
+    --ts_out=service=grpc-node,mode=grpc-js:api/usersd/pb/javascript \
   api/billingd/pb/billingd.proto api/usersd/pb/usersd.proto
 
 echo generating web-protos in api/usersd/pb/javascript/browser
-mkdir api/usersd/pb/javascript/browser 2>/dev/null
+mkdir -p api/usersd/pb/javascript/browser
 ./buildtools/protoc/bin/protoc \
   --proto_path=. \
   --plugin=protoc-gen-ts=api/usersd/pb/javascript/node_modules/.bin/protoc-gen-ts \
@@ -51,15 +53,16 @@ mkdir api/usersd/pb/javascript/browser 2>/dev/null
 
 echo generating node-protos in api/bucketsd/pb/javascript
 grpc_tools_node_protoc \
-  --js_out=import_style=commonjs,binary:api/bucketsd/pb/javascript \
-  --grpc_out=grpc_js:api/bucketsd/pb/javascript \
-  --plugin=protoc-gen-ts=api/bucketsd/pb/javascript/node_modules/.bin/protoc-gen-ts \
-  --ts_out=api/bucketsd/pb/javascript \
-  -I ./ \
+    -I=. \
+    --plugin=protoc-gen-ts=api/bucketsd/pb/javascript/node_modules/.bin/protoc-gen-ts \
+    --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+    --js_out=import_style=commonjs:api/bucketsd/pb/javascript \
+    --grpc_out=grpc_js:api/bucketsd/pb/javascript \
+    --ts_out=service=grpc-node,mode=grpc-js:api/bucketsd/pb/javascript \
   api/bucketsd/pb/bucketsd.proto
 
 echo generating web-protos in api/bucketsd/pb/javascript/browser
-mkdir api/bucketsd/pb/javascript/browser 2>/dev/null
+mkdir -p api/bucketsd/pb/javascript/browser
 ./buildtools/protoc/bin/protoc \
   --proto_path=. \
   --plugin=protoc-gen-ts=api/bucketsd/pb/javascript/node_modules/.bin/protoc-gen-ts \
