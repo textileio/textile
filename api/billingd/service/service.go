456766c6eb2dcd2cd3e0929a210616fce78616be
Andrew Hill <andrew@textile.io>
2020-11-30 18:34:32 -0800

    chore(lint): cleanup names
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- api/billingd/service/service.go
+++ api/billingd/service/service.go
@@ -480,7 +480,7 @@ func (s *Service) createCustomer(
 	}
 	log.Debugf("created customer %s with id %s", doc.Key, doc.CustomerID)
 
-	go s.analytics.CreateUser(doc.Key, doc.Email, map[string]string{
+	go s.analytics.NewUser(doc.Key, doc.Email, map[string]string{
 		"parent_key":                      doc.ParentKey,
 		"customer_id":                     doc.CustomerID,
 		"account_type":                    fmt.Sprint(doc.AccountType),
