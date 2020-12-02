5e2b2fb305f0bb3c3ec9ab53a24971f4d63467c0
Sander Pick <sanderpick@gmail.com>
2020-12-02 13:53:02 -0800

    billing: add db migrations
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

---
+++ api/billingd/migrations/migrations_test.go
@@ -0,0 +1,67 @@
+package migrations
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	migrate "github.com/xakep666/mongo-migrate"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Test make customer_id index sparse
+func TestMigrations_m001(t *testing.T) {
+	ctx := context.Background()
+	db := setup(t, ctx)
+
+	// Preload collection
+	_, err := db.Collection("customers").Indexes().CreateOne(ctx, mongo.IndexModel{
+		Keys:    bson.D{{"customer_id", 1}},
+		Options: options.Index().SetUnique(true),
+	})
+	require.NoError(t, err)
+	_, err = db.Collection("customers").InsertMany(ctx, []interface{}{
+		bson.M{"customer_id": "one"},
+		bson.M{"customer_id": "two"},
+		bson.M{"customer_id": "three"},
+	})
+	require.NoError(t, err)
+
+	// Test that nil customer_id causes duplicate key error
+	docs := []interface{}{
+		bson.M{"foo": 1}, // nil customer_id
+		bson.M{"bar": 1}, // nil customer_id
+	}
+	_, err = db.Collection("customers").InsertMany(ctx, docs)
+	require.Error(t, err) // Duplicate key error
+
+	// Run up
+	err = migrate.NewMigrate(db, m001).Up(migrate.AllAvailable)
+	require.NoError(t, err)
+
+	// No duplicate key error this time
+	_, err = db.Collection("customers").InsertMany(ctx, docs)
+	require.NoError(t, err)
+
+	// Clean up
+	_, err = db.Collection("customers").DeleteMany(ctx, bson.M{})
+	require.NoError(t, err)
+
+	// Run down
+	err = migrate.NewMigrate(db, m001).Down(migrate.AllAvailable)
+	require.NoError(t, err)
+}
+
+func setup(t *testing.T, ctx context.Context) *mongo.Database {
+	uri := "mongodb://127.0.0.1:27017"
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	require.NoError(t, err)
+	db := client.Database("test_billing_migrations")
+	t.Cleanup(func() {
+		err := db.Drop(ctx)
+		require.NoError(t, err)
+	})
+	return db
+}
