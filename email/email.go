d56c71a188273fd5412f86c0de3e8dc7ea4de983
Sander Pick <sanderpick@gmail.com>
2020-12-10 22:16:09 -0800

    email: cleanup package
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- email/email.go
+++ email/email.go
@@ -4,23 +4,19 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/customerio/go-customerio"
+	cio "github.com/customerio/go-customerio"
 	logging "github.com/ipfs/go-log"
 	"github.com/textileio/go-threads/util"
 )
 
-var (
-	log = logging.Logger("email")
-)
+var log = logging.Logger("email")
 
-// Client service.
 type Client struct {
 	inviteTmpl  string
 	confirmTmpl string
-	client      *customerio.APIClient
+	client      *cio.APIClient
 }
 
-// Config defines the client configuration.
 type Config struct {
 	ConfirmTmpl string
 	InviteTmpl  string
@@ -28,7 +24,6 @@ type Config struct {
 	Debug       bool
 }
 
-// NewClient return a email api client.
 func NewClient(conf Config) (*Client, error) {
 	if conf.Debug {
 		if err := util.SetLogLevels(map[string]logging.LogLevel{
@@ -38,29 +33,25 @@ func NewClient(conf Config) (*Client, error) {
 		}
 	}
 
-	var client *customerio.APIClient
+	var client *cio.APIClient
 	if conf.APIKey != "" {
-		client = customerio.NewAPIClient(conf.APIKey)
+		client = cio.NewAPIClient(conf.APIKey)
 	}
-
-	api := &Client{
+	return &Client{
 		inviteTmpl:  conf.InviteTmpl,
 		confirmTmpl: conf.ConfirmTmpl,
 		client:      client,
-	}
-
-	return api, nil
+	}, nil
 }
 
 // ConfirmAddress sends a confirmation link to a recipient.
-func (sg *Client) ConfirmAddress(ctx context.Context, id, username, email, url, secret string) error {
-	if sg.client == nil {
-		log.Debug("Skipping email send")
+func (c *Client) ConfirmAddress(ctx context.Context, id, username, email, url, secret string) error {
+	if c.client == nil {
 		return nil
 	}
-	request := customerio.SendEmailRequest{
-		To:                     email,
-		TransactionalMessageID: sg.confirmTmpl,
+	request := cio.SendEmailRequest{
+		To: email,
+		TransactionalMessageID: c.confirmTmpl,
 		Identifiers: map[string]string{
 			"id":           id,
 			"confirmation": "true",
@@ -70,37 +61,36 @@ func (sg *Client) ConfirmAddress(ctx context.Context, id, username, email, url, 
 			"username": username,
 		},
 	}
-
-	_, err := sg.client.SendEmail(ctx, &request)
-	if err != nil {
-		log.Fatal(err)
+	if _, err := c.client.SendEmail(ctx, &request); err != nil {
+		return err
 	}
-	return err
+
+	log.Debug("sent confirm address for %s to %s", username, email)
+	return nil
 }
 
 // InviteAddress sends a confirmation link to a recipient.
-func (sg *Client) InviteAddress(ctx context.Context, id, org, email, to, url, token string) error {
-	if sg.client == nil {
-		log.Debug("Skipping email send")
+func (c *Client) InviteAddress(ctx context.Context, id, org, from, to, url, token string) error {
+	if c.client == nil {
 		return nil
 	}
-	request := customerio.SendEmailRequest{
-		To:                     to,
-		TransactionalMessageID: sg.inviteTmpl,
+	request := cio.SendEmailRequest{
+		To: to,
+		TransactionalMessageID: c.inviteTmpl,
 		Identifiers: map[string]string{
 			"id":    id,
-			"email": email,
+			"email": from,
 		},
 		MessageData: map[string]interface{}{
 			"link": fmt.Sprintf("%s/consent/%s", url, token),
 			"org":  org,
-			"from": email,
+			"from": from,
 		},
 	}
-
-	_, err := sg.client.SendEmail(ctx, &request)
-	if err != nil {
-		log.Fatal(err)
+	if _, err := c.client.SendEmail(ctx, &request); err != nil {
+		return err
 	}
-	return err
+
+	log.Debug("sent invite to %s from %s to %s", org, from, to)
+	return nil
 }
